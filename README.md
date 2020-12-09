f890b98cf7b6f313ef7dc614c19b1411448f38a3
HUANCHENZH <55564232+HUANCHENZH@users.noreply.github.com>
2020-12-09 23:34:24 +0800

    update

---
+++ README.md
@@ -0,0 +1,2 @@
+# mi349_blog
+# Preproduction works have been formatted into a single document in pdf version
---
+++ soundboard.js
@@ -0,0 +1,13 @@
+var getarrow = document.getElementById("arrowsound");
+
+var getboing = document.getElementById("bosound");
+
+function arrow() {
+    getarrow.play();
+}
+
+
+function arrowenter() {
+    getboing.play()
+}
+
